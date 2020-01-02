21211d7db819d057ac5f54295a70e807f14430fc
huhongyun <xiaomiwujiecao@gmail.com>
2020-01-02 19:11:24 +0800

    1. 添加忽略文件 2. 修复启用插件后，无法加载前台js文件问题 3. 修复加载多个插件时，jQuery没有准备完成，无效果的问题

--- codecopy.js
+++ codecopy.js
@@ -1,9 +1,11 @@
-//html5 给typecho添加 复制代码 功能 
+//html5 给typecho添加 复制代码 功能
 // by 兔子昂
-var codeblocks = document.getElementsByTagName("pre")
+// fixed by xiaomiwujiecao
+$(function () {
+  var codeblocks = document.getElementsByTagName("pre")
 //循环每个pre代码块，并添加 复制代码
 
-for (var i = 0; i < codeblocks.length; i++) {
+  for (var i = 0; i < codeblocks.length; i++) {
     //显示 复制代码 按钮
     currentCode = codeblocks[i]
     currentCode.style = "position: relative;"
@@ -16,46 +18,49 @@ for (var i = 0; i < codeblocks.length; i++) {
     currentCode.appendChild(copy)
     //让所有 "复制"按钮 全部隐藏
     copy.style.visibility = "hidden"
-}
+  }
 
 
-for (var i = 0; i < codeblocks.length; i++) {
+  for (var i = 0; i < codeblocks.length; i++) {
 
 
     !function (i) {
-        //鼠标移到代码块，就显示按钮
-        codeblocks[i].onmouseover = function () {
-            codeblocks[i].childNodes[1].style.visibility = "visible"
-        }
+      //鼠标移到代码块，就显示按钮
+      codeblocks[i].onmouseover = function () {
+        codeblocks[i].childNodes[1].style.visibility = "visible"
+      }
 
-        //执行 复制代码 功能
-        function copyArticle(event) {
-            const range = document.createRange();
+      //执行 复制代码 功能
+      function copyArticle(event) {
+        const range = document.createRange();
 
-            //范围是 code，不包括刚才创建的div
-            range.selectNode(codeblocks[i].childNodes[0]);
+        //范围是 code，不包括刚才创建的div
+        range.selectNode(codeblocks[i].childNodes[0]);
 
-            const selection = window.getSelection();
-            if (selection.rangeCount > 0) selection.removeAllRanges();
-            selection.addRange(range);
-            document.execCommand('copy');
+        const selection = window.getSelection();
+        if (selection.rangeCount > 0) selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand('copy');
 
-            codeblocks[i].childNodes[1].innerHTML = "复制成功"
-            setTimeout(function () {
-                codeblocks[i].childNodes[1].innerHTML = "复制"
-            }, 1000);
-            //清除选择区
-            if (selection.rangeCount > 0) selection.removeAllRanges(); 0
-        }
-        codeblocks[i].childNodes[1].addEventListener('click', copyArticle, false);
+        codeblocks[i].childNodes[1].innerHTML = "复制成功"
+        setTimeout(function () {
+          codeblocks[i].childNodes[1].innerHTML = "复制"
+        }, 1000);
+        //清除选择区
+        if (selection.rangeCount > 0) selection.removeAllRanges();
+        0
+      }
+
+      codeblocks[i].childNodes[1].addEventListener('click', copyArticle, false);
 
     }(i);
 
     !function (i) {
-        //鼠标从代码块移开 则不显示复制代码按钮
-        codeblocks[i].onmouseout = function () {
-            codeblocks[i].childNodes[1].style.visibility = "hidden"
-        }
+      //鼠标从代码块移开 则不显示复制代码按钮
+      codeblocks[i].onmouseout = function () {
+        codeblocks[i].childNodes[1].style.visibility = "hidden"
+      }
     }(i);
-}
+  }
+})
 
